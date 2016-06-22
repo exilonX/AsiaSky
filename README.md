ebc7931c6446e789d0fe412d1e2177972f781ec8
exilonX <ionel.merca@gmail.com>
2016-06-22 16:55:57 +0300

    Updated readme and main

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AsiaSky
-Find best prices for airline tickets for a 6 month travel through South East Asia using the skyScanner api. 
+Find the cheapest airline ticket between two airports.
+Input :
+  - Departure Airport
+  - Arrival Airport
+  - First day of departure from departure airport
+  - First day of departure from arrival airport
+
+Search using the skyscanner api for live prices from First Day of departure from departure airport + 30  to First day of departure from arrival airport + 30, looking for the best price with every combination of dates.
+
+Output:
+  - The cheapest flight
+  - The departure dates
+  - Link to buy the ticket 
--- app/asiasky.js
+++ app/asiasky.js
@@ -21,54 +21,6 @@ const defaultParamsLivePrice = {
 
 const ipAddr = "127.0.0.1";
 
-let skyScanner = new SkyScanner.Skyscanner(API_KEY);
-var countryList = ['Thailand', 'Vietnam', 'Cambodia', 'Indonesia', 'Malaysia', 'Philippines'];
-
-// skyScanner.autosuggest({
-//   market : 'RO',
-//   currency : 'EUR',
-//   locale: "en-GB"
-// }, 'Bangkok')
-//   .then((response) => {
-//     console.log(response.data);
-//   })
-
-var query = {
-  market: "RO",
-  currency: "EUR",
-  locale: "en-GB",
-  originPlace : "RO",
-  destinationPlace : "TH",
-  outboundPartialDate : "2016-10-01"
-}
-
-// skyScanner.flights.browse.routes(query, ipAddr)
-//   .then((response) => {
-//     console.log(response.data);
-//   })
-//   .catch((response) => {
-//     if (response instanceof Error) {
-//       console.log("Error ", response.message);
-//     } else {
-//       console.log("Error thrown");
-//       console.log(response.data);
-//       console.log(response.status);
-//       console.log(response.headers);
-//       console.log(response.config);
-//     }
-//   })
-
-// async.waterfall([
-//   ((cb) => {
-//
-//   }),
-//   ((countries, cb) => {
-//     console.log("The countries are", countries);
-//     cb(null);
-//   })
-// ], function(err, data) {
-//   console.log("Se termina");
-// })
 
 let firstFlight = new FirstFlight(API_KEY, "2016-10-01", "2017-03-01", defaultParamsLivePrice, 'OTP-sky', 'BKKT-sky');
 
